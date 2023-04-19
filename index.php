21a63a4c533bafa693687c2ce79198aa6e937cec
gashishishi <wawwooaz@gmail.com>
2023-04-19 12:51:54 +0900

    fixed

--- index.php
+++ index.php
@@ -203,10 +203,12 @@
         const fixedHeight = $('nav').height();
         var scroll;
 
-        var objTop = $('.work-fixed').offset().top;
+        var workImgTop = $('.work-fixed').offset().top;
+        var workBottom = $('.work').offset().top + $('.work').outerHeight();;
         $(window).on('scroll', function(){
             scroll = $(window).scrollTop();
-        if(scroll >= objTop - fixedHeight){
+        // work-fixedがnavの位置まで来たら固定する
+        if(scroll >= workImgTop - fixedHeight){
             console.log(scroll);
             if(!$('.work-fixed').hasClass('active')){
                 $('.work-fixed').addClass('active')
@@ -215,6 +217,19 @@
             if($('.work-fixed').addClass('active')){
                 $('.work-fixed').removeClass('active')
             }
+        }
+
+        // workを通り過ぎたら hero-bg-img,work-bg-imgをd-noneにする。
+        const heroImg = $('.hero-bg-img');
+        const workImg = $('.work-bg-img');
+        if(scroll >= workBottom){
+            if (!heroImg.hasClass('d-none'))
+                heroImg.addClass('d-none');
+                workImg.addClass('d-none');
+        } else {
+            if (heroImg.hasClass('d-none'))
+                heroImg.removeClass('d-none');
+                workImg.removeClass('d-none');
         }
         });
     });
